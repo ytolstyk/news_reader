89e4a322c59d5efa2506e7cc826cce23c7c659b8
App Academy Student <student@appacademy.io>
2014-09-16 17:48:19 -0700

    user model

--- skeleton/app/models/user.rb
+++ skeleton/app/models/user.rb
@@ -1,12 +1,27 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string(255)      not null
+#  password_digest :string(255)      not null
+#  session_token   :string(255)      not null
+#  created_at      :datetime
+#  updated_at      :datetime
+#
+
 class User < ActiveRecord::Base
   validates :username, :password_digest, :session_token, presence: true
   validates :password, length: { minimum: 6, allow_nil: true }
+  
   after_initialize :ensure_session_token
   
   attr_reader :password
   
   def self.find_by_credentials(username, password)
-    
+    user = User.find_by_username(username)
+    return nil if user.nil?
+    self.is_password?(password) ? user : nil
   end
   
   def self.generate_session_token
@@ -15,7 +30,11 @@ class User < ActiveRecord::Base
   
   def password=(password)
     @password = password
-    self.password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+  
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
   end
   
   private
@@ -23,6 +42,4 @@ class User < ActiveRecord::Base
   def ensure_session_token
     self.session_token ||= self.class.generate_session_token
   end
-  
-  
 end
